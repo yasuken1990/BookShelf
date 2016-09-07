9cb11dc7646fed7d720fee2430c57270936720ce
Kenji Yasumatsu <yasuken1990@gmail.com>
2016-09-07 12:40:33 +0900

    [add]save()追加

--- app/BookShelf.php
+++ app/BookShelf.php
@@ -30,5 +30,49 @@ class BookShelf
     {
         $this->books[] = $book;
     }
+    public function save($type = null)
+    {
+	if ($type === null) {
+            $this->dispBook();
+        }
+            $export = $type;
+            $this->$export();
+	}
+    }
 
+    // フォーマットの指定がないのでとりあえずprint_rしとく
+    public function dispBook()
+    {
+        print_r($this->books);
+    }
+
+    // 配列名の*が気持ち悪いけど仕方ない？
+    public function csv()
+    {
+        $csv = '';
+        $col = '';
+
+        foreach ($this->books as $key => $book) {
+            foreach ((array)$book as $key => $value) {
+                $csv .= "{$value},";
+            }
+            $csv = preg_replace("#,$#misu", "", $csv);
+            $csv .= "\n"; 
+        }
+
+        foreach (array_keys((array)$book) as $bookCol) {
+            $col .= (string)$bookCol . ',';
+        }
+        $col = preg_replace("#,$#misu", "", $col);
+        $col .= "\n"; 
+
+        $csv = $col . $csv;
+
+        file_put_contents($this->savePath . 'data.csv', $csv);
+    }
+
+    // 気が向いたら実装
+    public function json()
+    {
+    }
 }
